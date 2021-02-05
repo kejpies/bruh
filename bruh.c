1e2e6826677c0dec90c960d2d6d513ad429d7b82
kejpies <konradsekula915@gmail.com>
2021-02-05 20:29:48 +0100

    Repairing switch-from-python mistakes

--- bruh.c
+++ bruh.c
@@ -1,2 +1,2 @@
 // Copyright 2020-2021 Konrad Sekuła
-int main(){if system("aplay /usr/share/bruh/bruh.wav &> /dev/null")!=0 system("paplay /usr/share/bruh/bruh.wav &> /dev/null");}
+int main(){if (system("aplay /usr/share/bruh/bruh.wav &> /dev/null")!=0) system("paplay /usr/share/bruh/bruh.wav &> /dev/null");}
